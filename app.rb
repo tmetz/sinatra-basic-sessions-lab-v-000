3d2a725979dc8340f0a2d503c3e09701f39641bc
Tammy Metz <tammy@tammymetz.com>
2018-11-23 22:08:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, ""
+    set :session_secret, "b137cc566e9cfd74cb85056960326971f80db615dd13575cfea5cdc9a081dd36b225e84e75dbc9aedeba99796fb86d8ef2a73d252ae648d587fc857246bc239d"
   end
 
   get "/" do
