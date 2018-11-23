50c9e9427e438dda9e667002c483056a8928a6c8
Tammy Metz <tammy@tammymetz.com>
2018-11-23 22:02:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :sessions
+    set :session_secret, ""
+  end
+
   get "/" do
     erb :index
   end
 
   post "/checkout" do
-    
+    session["item"] = params["item"]
+    @session = session
   end
 end
